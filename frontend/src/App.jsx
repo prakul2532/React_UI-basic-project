f4e1c4e4603e1cf2634bac512c2b07454056ce2d
Student-Prakul <prakul25.doe.svc.du@gmail.com>
2026-01-31 17:46:32 +0530

    removed unnecessary statements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,9 +11,9 @@ function App() {
       <Block Video_Title="2-HOUR Rainy Night Walk in Shibuya,Tokyo" Video_Src="sAkVnhthpMI" channel_name="Abao Ambience" rate="3"></Block>
       <Block Video_Title="2.5-HOUR STUDY WITH ME" Video_Src="sUwD3GRPJos" channel_name="Abao in Tokyo" rate="4"></Block>
       <Block Video_Title="music for your mind to rest" Video_Src="d3ZC-9YKBIM" channel_name="bliss and Mylesxiety" rate="3"></Block>
-      <Block Video_Title="Ø𝐧𝐞𝐡𝐞𝐚𝐫𝐭 & 𝐑𝐞𝐢𝐝𝐞𝐧𝐬𝐡𝐢-𝐒𝐧𝐨𝐰𝐟𝐚𝐥𝐥(𝐬𝐥𝐨𝐰𝐞𝐝)" Video_Src="vZZTerW8Llc" channel_name="DriftDynamosYT" rate="4"></Block>
-      <Block Video_Title="777 Hz Third Eye Activation Meditation" Video_Src="15tVFFGsI1E" channel_name="Etheric Meditation" rate="5" ></Block>
-      <Block Video_Title="The most powerful frequency of the universe 999 Hz" Video_Src="-pu93aq9uP4" channel_name="Lucid Dreaming Music" rate="5" ></Block>
+      <Block Video_Title="Ø𝐧𝐞𝐡𝐞𝐚𝐫𝐭 & Reindenshi(slowed)" Video_Src="vZZTerW8Llc" channel_name="DriftDynamosYT" rate="4"></Block>
+      <Block Video_Title="Third Eye Activation Meditation" Video_Src="15tVFFGsI1E" channel_name="Etheric Meditation" rate="5" ></Block>
+      <Block Video_Title="powerful frequency of the universe" Video_Src="-pu93aq9uP4" channel_name="Lucid Dreaming Music" rate="5" ></Block>
       <Block Video_Title="Night drive relax - nissan titan warrior" Video_Src="yoCX1BJKNcM" channel_name="Carlajestica" rate="4" ></Block>
       <Block Video_Title="pov: you're on a long drive in john wick's car" Video_Src="gJy6nCMOjTk" channel_name="ForzaRush" rate="5" ></Block>
       <Block Video_Title="Cyberpunk Lofi for Late Nights" Video_Src="M23hGVK-oYI" channel_name="Chilora" rate="3" ></Block>
